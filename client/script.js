7c461079e7ff0f7f55c405849d14c59e2c126883
Alcatraz Deve <alcatraz3019@gmail.com>
2022-12-24 02:50:15 +0100

    Update the server Url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codex-im0y.onrender.com/", {
+  const response = await fetch("https://aicodex.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
