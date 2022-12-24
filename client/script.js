d4eb2b33daaf065cb91f596f31f3d2c7549c551c
Alcatraz Deve <alcatraz3019@gmail.com>
2022-12-24 05:56:10 +0100

    update

--- client/script.js
+++ client/script.js
@@ -84,8 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await ("https://aicodex.onrender.com",
-  {
+  const response = await fetch("https://aicodex.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
